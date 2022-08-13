af9f759653e3d5f91eda61daf4bd0789d1994935
G1gg1L3s <mr.a678i@gmail.com>
2022-08-13 20:59:37 +0300

    Make compilable

--- applet/src/test/java/tests/AppletTest.java
+++ applet/src/test/java/tests/AppletTest.java
@@ -1,6 +1,10 @@
 package tests;
 
+import applet.HelloWorldApplet;
+import com.licel.jcardsim.smartcardio.CardSimulator;
+import com.licel.jcardsim.utils.AIDUtil;
 import cz.muni.fi.crocs.rcard.client.CardType;
+import javacard.framework.AID;
 import org.junit.Assert;
 import org.junit.jupiter.api.*;
 
@@ -14,7 +18,7 @@ import javax.smartcardio.ResponseAPDU;
  * @author xsvenda, Dusan Klinec (ph4r05)
  */
 public class AppletTest extends BaseTest {
-    
+
     public AppletTest() {
         // Change card type here if you want to use physical card
         setCardType(CardType.JCARDSIMLOCAL);
@@ -44,5 +48,26 @@ public class AppletTest extends BaseTest {
         Assert.assertNotNull(responseAPDU);
         Assert.assertEquals(0x9000, responseAPDU.getSW());
         Assert.assertNotNull(responseAPDU.getBytes());
+    }
+
+    @Test
+    public void checkHello() {
+        // 1. create simulator
+        CardSimulator simulator = new CardSimulator();
+
+        // 2. install applet
+        AID appletAID = AIDUtil.create("F000000001");
+        simulator.installApplet(appletAID, HelloWorldApplet.class);
+
+        // 3. select applet
+        simulator.selectApplet(appletAID);
+
+        // 4. send APDU
+        CommandAPDU commandAPDU = new CommandAPDU(0x00, 0x90, 0x00, 0x00);
+        ResponseAPDU response = simulator.transmitCommand(commandAPDU);
+
+        String res = new String(response.getData());
+
+        Assert.assertEquals(res, "Hello World!");
     }
 }
