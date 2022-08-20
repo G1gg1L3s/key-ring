10e217c9135b069ec37b778a84a77e1186f809f2
G1gg1L3s <mr.a678i@gmail.com>
2022-08-20 19:26:17 +0300

    Add rng and a couple tests

--- applet/src/main/java/applet/crypto/CryptoApplet.java
+++ applet/src/main/java/applet/crypto/CryptoApplet.java
@@ -6,17 +6,28 @@ public class CryptoApplet extends Applet {
     final public static byte INS_HMAC_SHA256 = 0x25;
     final public static byte INS_AES_CTR_ENC = 0x26;
     final public static byte INS_AES_CTR_DEC = 0x27;
+    final public static byte INS_RAND = 0x28;
     public static byte[] buffer;
 
     public static void install(byte[] bArray, short bOffset, byte bLength) {
         new CryptoApplet().register();
         HmacSha256.init();
         AesCtr.init(JCSystem.makeTransientByteArray(AesCtr.REQUIRED_BUFFER_LENGTH, JCSystem.CLEAR_ON_DESELECT));
+        Rng.init(
+                new byte[Rng.SEED_SIZE],
+                JCSystem.makeTransientByteArray(Rng.REQUIRED_BUFFER_SIZE, JCSystem.CLEAR_ON_DESELECT)
+        );
         buffer = JCSystem.makeTransientByteArray((short) 1024, JCSystem.CLEAR_ON_DESELECT);
     }
 
     public CryptoApplet() {
 
+    }
+
+    @Override
+    public boolean select() {
+        Rng.reseed();
+        return true;
     }
 
     public void hmacSha256(APDU apdu) {
@@ -68,6 +79,17 @@ public class CryptoApplet extends Applet {
         apdu.setOutgoingAndSend(dataOffset, dataLength);
     }
 
+    public void rand(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        apdu.setIncomingAndReceive();
+
+        // we expect only 2-byte length, which specifies number of bytes
+        // to return
+        short len = Util.getShort(buffer, ISO7816.OFFSET_CDATA);
+        Rng.fill(buffer, ISO7816.OFFSET_CDATA, len);
+        apdu.setOutgoingAndSend(ISO7816.OFFSET_CDATA, len);
+    }
+
     public void process(APDU apdu) {
         byte[] buffer = apdu.getBuffer();
 
@@ -79,6 +101,9 @@ public class CryptoApplet extends Applet {
             case INS_AES_CTR_DEC:
                 boolean encrypt = buffer[ISO7816.OFFSET_INS] == INS_AES_CTR_ENC;
                 aes_ctr(apdu, encrypt);
+                break;
+            case INS_RAND:
+                rand(apdu);
                 break;
             default:
                 ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
---
+++ applet/src/test/java/tests/RngTest.java
@@ -0,0 +1,60 @@
+package tests;
+
+import applet.crypto.CryptoApplet;
+import javacard.framework.AID;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import javax.smartcardio.CommandAPDU;
+import javax.smartcardio.ResponseAPDU;
+import java.util.HashSet;
+import java.util.Set;
+
+public class RngTest extends CryptoBase {
+    byte[] rand(short len) {
+        byte[] apduData = new byte[2];
+        putShort((short) len, apduData, 0);
+        CommandAPDU apdu = new CommandAPDU(0x00, CryptoApplet.INS_RAND, 0x00, 0x00, apduData);
+        ResponseAPDU response = card.transmitCommand(apdu);
+        return response.getData();
+    }
+
+    @Test
+    public void checkLength() {
+        for (short i = 0; i < 128; i++) {
+            byte[] random = rand(i);
+            Assert.assertEquals(i, random.length);
+        }
+    }
+
+    @Test
+    public void dontRepeatAfterDeselect() {
+        Set<byte[]> set = new HashSet<>();
+
+        int numberOfRequests = 128;
+
+        card.selectApplet(aid);
+        for (int i = 0; i < numberOfRequests; i++) {
+            set.add(rand((short) 32));
+        }
+        card.selectApplet(aid);
+        for (int i = 0; i < numberOfRequests; i++) {
+            set.add(rand((short) 32));
+        }
+        Assert.assertEquals(set.size(), numberOfRequests * 2);
+    }
+
+    @Test
+    public void dontRepeatAfterReseed() {
+        Set<byte[]> set = new HashSet<>();
+
+        int numberOfRequests = 1024;
+
+        card.selectApplet(aid);
+        for (int i = 0; i < numberOfRequests; i++) {
+            set.add(rand((short) 32));
+        }
+
+        Assert.assertEquals(set.size(), numberOfRequests);
+    }
+}
