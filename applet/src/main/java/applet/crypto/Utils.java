03c8f5b075fbf7a6f0e7f6e6cbbc8e10f619d016
G1gg1L3s <mr.a678i@gmail.com>
2022-08-20 21:14:06 +0300

    Add constant time comparison

--- applet/src/main/java/applet/crypto/Utils.java
+++ applet/src/main/java/applet/crypto/Utils.java
@@ -2,11 +2,18 @@ package applet.crypto;
 
 public class Utils {
 
+    public static boolean const_eq(byte[] a, short offsetA, byte[] b, short offsetB, short length) {
+        byte result = 0;
+        for (byte i = 0; i < length; i++) {
+            result |= (byte) (a[(short) (offsetA + i)] ^ b[(short) (offsetB + i)]);
+        }
+        return result == 0;
+    }
+
     public static void xor(
             byte[] src, short srcOffset,
             byte[] dst, short dstOffset,
-            short len
-    ) {
+            short len) {
         for (short i = 0; i < len; i++) {
             dst[(short) (dstOffset + i)] ^= src[(short) (srcOffset + i)];
         }
